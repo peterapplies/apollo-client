4110e86c776533912a05f1863d7e93c9ff95a657
peyo <peteryyoon@gmail.com>
2020-11-23 20:00:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+import React from "react";
+
+const LINKS = gql`
+  query GetLinks {
+    getLinks(id: "XXXX") {
+      name
+      url
+      slug
+    }
+  }
+`;
+
+function Links() {
+  const { loading, error, data } = useQuery(LINKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getLinks.map(({ name, url, slug }) => (
+    <div key={slug}>
+      <p>
+        {name}
+      </p>
+      <p>
+        {url}
+      </p>
+      <p>
+        {slug}
+      </p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Links />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Apollo
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+// GraphQL
+import { gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://uqjqp.sse.codesandbox.io/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
